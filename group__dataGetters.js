f54b83ef27662ed9b984f969d1fa621a63959b26
Jim Derry <balthisar@gmail.com>
2014-02-28 18:37:27 +0800

    Regen dox.

--- group__dataGetters.js
+++
@@ -1,6 +0,0 @@
-var group__dataGetters =
-[
-    [ "getPageNavigatorDataForBook", "group__dataGetters.html#ga2a99b360bec576d23b480c4e31ee9d93", null ],
-    [ "getSitemapDataForBook", "group__dataGetters.html#gae784a24d6ca4df3d5b1828418dfd03bd", null ],
-    [ "getTocDataForBook", "group__dataGetters.html#ga5214168a919f5c7de16066894c5835e6", null ]
-];
--- modules.js
+++
@@ -1,4 +0,0 @@
-var modules =
-[
-    [ "Methods that fetch data structures for use.", "group__dataGetters.html", "group__dataGetters" ]
-];
